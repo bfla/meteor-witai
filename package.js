d9ff30802fcbf31fb4094b7989195270212cd44d
Brian <brianflaherty64@gmail.com>
2016-02-19 15:05:02 -0500

    updated package.js so that package builds correctly

--- package.js
+++ package.js
@@ -11,7 +11,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.export('WitaiClient');
+  // api.export('WitaiClient');
   api.versionsFrom('1.2.1');
 
   // api.use('ecmascript');
@@ -19,18 +19,22 @@ Package.onUse(function(api) {
   // api.use("isobuild:cordova@5.2.0");
 
   // Include official Wit.ai browser integration
-  api.addFiles('browser/witai-browser-client/witai.min.css', ['web.browser']);
-  api.addfiles('browser/witai-browser-client/witai.min.js', ['web.browser']);
-  api.addFiles('browser/witai-browser-client/fonts/microphone.eot', ['web.browser'], {isAsset:true});
-  api.addFiles('browser/witai-browser-client/fonts/microphone.svg', ['web.browser'], {isAsset:true});
-  api.addFiles('browser/witai-browser-client/fonts/microphone.ttf', ['web.browser'], {isAsset:true});
-  api.addFiles('browser/witai-browser-client/fonts/microphone.woff', ['web.browser'], {isAsset:true});
+  api.addFiles('browser/witai-browser-client/microphone.min.css', ['web.browser']);
+  api.addFiles('browser/witai-browser-client/microphone.min.js', ['web.browser']);
+  api.addFiles(
+    [
+      'browser/witai-browser-client/fonts/microphone.eot',
+      'browser/witai-browser-client/fonts/microphone.svg',
+      'browser/witai-browser-client/fonts/microphone.ttf',
+      'browser/witai-browser-client/fonts/microphone.woff'
+    ], 
+    ['web.browser'], 
+    {isAsset:true}
+  );
   // Include Meteor interface for Wit.ai's browser integration
   api.addFiles('browser/witai-interface.html', ['web.browser']);
   api.addFiles('browser/witai-interface.js', ['web.browser']);
 
-  // api.addFiles('witai.less', ['web.browser']);
-  // api.addFiles('witai-browser.js', ['web.browser']);
   // api.addFiles('witai-cordova.js', ['web.cordova']);
 });
 
